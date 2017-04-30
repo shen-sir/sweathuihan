16c79350dde97744f0126161c30fa05c6f5d4ab9
shen <shen@macdeMacBook-Pro-4.local>
2017-04-30 19:26:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+input{
+  -webkit-appearance:none;
+  outline:none;
+}
+input:focus{
+    -webkit-tap-highlight-color:rgba(0,0,0,0);
+    -webkit-user-modify:read-write-plaintext-only;
+}
+*:focus {outline: none;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+}
+html{
+  font-size: calc(100vw/3.75);
+}
+body{
+  font-size: 0.15rem;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <div class="search">
+      <input placeholder="查找附近场馆" type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='less' scoped >
+div.hello{
+  border: 1px solid red;
+  height: 100%;
+  .search{
+    width: 100%;
+    height: .35rem;
+    background: #ededed;
+    margin: 0;
+    input{
+      width: 80%;
+      background: #ededed;
+      border: 1px solid black;
+      margin-top: .06rem;
+      margin-left: .09rem;
+      text-align: center;
+    }
+  }
+}
+</style>
